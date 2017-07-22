969f4c73ac5bcd8aac33689dfb099c3f8f2c8318
tothemoonsands <tothemoonsands@users.noreply.github.com>
2017-07-21 23:11:10 -0500

    Add Concatenation

---
+++ css/home.combined.js
@@ -0,0 +1,7 @@
+<!--#include file="js/jquery-3.1.1.min.js" -->
+<!--#include file="js/parallax.js" -->
+<!--#include file="js/smooth-scroll.min.js" -->
+<!--#include file="js/scripts.js" -->
+<!--#include file="js/flickity.min.js" -->
+<!--#include file="js/granim.min.js" -->
+<!--#include file="js/twitterfetcher.min.js" -->
---
+++ css/slack.combined.js
@@ -0,0 +1,4 @@
+<!--#include file="js/jquery-3.1.1.min.js" -->
+<!--#include file="js/parallax.js" -->
+<!--#include file="js/smooth-scroll.min.js" -->
+<!--#include file="js/scripts.js" -->
